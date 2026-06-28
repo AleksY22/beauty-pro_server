fd7f44c3651d558578aff2cb1f85f811d223bcda
AleksY22 <71137788+AleksY22@users.noreply.github.com>
2026-06-28 20:54:19 +0300

    change main.ts

--- src/main.ts
+++ src/main.ts
@@ -59,7 +59,8 @@ async function bootstrap() {
    );
 
    app.enableCors({
-      origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+      // origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+      origin: '*',
       credentials: true,
       // exposedHeaders: ['set-cookie'],
    });
